b7b61700a603e2d59475f1afea427a5823d23d74
wangbo9988 <465204107@qq.com>
2020-01-09 17:13:34 +0800

    SpringCloud-Hoxton的学习,第一次提交，主要包含Eureka配置注册和服务中心，搭建Eureka注册集群，Eureka安全验证注册中心。

---
+++ eureka-security-server/src/main/java/com/cn/bo/config/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.cn.bo.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @ClassName WebSecurityConfig
+ * @Description TOD0
+ * @Author mr.wang
+ * @Date 2020/1/9 4:19 下午
+ * @Version 1.0
+ */
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+    }
+}
